93eb40f6d16d1ceeb6af626303b76a3d67ef9231
Maykon Bogéa <TDMaykongm@gmail.com>
2024-08-04 12:39:56 -0300

    mudei aqui

--- server.js
+++ server.js
@@ -6,3 +6,9 @@ const PORT = process.env.PORT||3000;
 app.listen(PORT, () => {
   console.log("servidor escutando!");
 });
+
+app.use((req,res, next) => {
+  res.header("Assess-Control-Allow-Origin", "http://127.0.0.1:5500");
+  res.header("Assess-Control-Allow-Headers", "Origin, X-Requested-With, Content-type, Accept");
+  next();
+});
